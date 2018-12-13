930ce5d93da6e3befc9ce271a3554d7ebf02abd3
Maurizio Pillitu <maoo@apache.org>
2018-12-13 15:44:51 +0100

    protobuf setup

--- install-protobuf.sh
+++ install-protobuf.sh
@@ -2,6 +2,6 @@
 
 wget https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOBUF_VERSION}/protobuf-all-${PROTOBUF_VERSION}.tar.gz
 
-tar -xzvf protobuf-all-${PROTOBUF_VERSION}.tar.gz
+tar -xzf protobuf-all-${PROTOBUF_VERSION}.tar.gz
 
 pushd protobuf-${PROTOBUF_VERSION} && ./configure --prefix=/usr && make && sudo make install && popd
