4c4960385f9bc44b08548fc2fbce522bd6197546
Maurizio Pillitu <maoo@apache.org>
2018-12-13 23:48:33 +0100

    update

--- install-protobuf.sh
+++ install-protobuf.sh
@@ -4,4 +4,4 @@ wget https://github.com/protocolbuffers/protobuf/releases/download/v${PROTOBUF_V
 
 tar -xzf protobuf-all-${PROTOBUF_VERSION}.tar.gz
 
-pushd protobuf-${PROTOBUF_VERSION} && ./configure --prefix=/usr && make && sudo make install && popd
+pushd protobuf-${PROTOBUF_VERSION} && ./configure --prefix=/usr && make && sudo make install && ls -l && popd
